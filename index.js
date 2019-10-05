ff13a0b954af8114af41aae77580b1e3edd8c65a
kmolitor <fwarrior.skm@gmail.com>
2019-10-05 12:08:39 +0500

    added basic feed object generation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Zen-feed by Ismail Valiev (Konrad Molitor) 2019
 // Distributed under the terms of MIT License
 // https://github.com/konrad-molitor/zen-feed
+const convert = require('xml-js');
 
 const configTemplate = require('./templates/configTemplate');
 const itemTemplate = require('./templates/itemTemplate');
@@ -10,7 +11,8 @@ const feed = {
     configure,
     configTemplate,
     itemTemplate,
-    filterContent
+    filterContent,
+    generateFeed
 }
 
 async function makeFeed (req, res, next){
@@ -29,6 +31,81 @@ async function makeFeed (req, res, next){
         }
         next()
     }    
+}
+
+function generateFeed(config, items){
+    let feed = {
+        elements: [
+            {
+                name: "rss",
+                type: "element",
+                attributes: {
+                    version: config.rssVersion,
+                    'xmlns:atom': "http://www.w3.org/2005/Atom",
+                    'xmlns:content': "http://purl.org/rss/1.0/modules/content/",
+                    'xmlns:dc': "http://purl.org/dc/elements/1.1/",
+                    'xmlns:georss': "http://www.georss.org/georss",
+                    'xmlns:media': "http://search.yahoo.com/mrss/"
+                },
+                elements: []
+            }
+        ]
+    }
+    const textElement  = (name, text) => {
+        return {
+            name,
+            type: "element",
+            elements: [
+                {
+                    text,
+                    type: "text"
+                }
+            ]
+        }
+    };
+    let channelElement = {
+        name: "channel",
+        type: "element",
+        elements: []
+    };
+    // add title
+    let titleElement = textElement("title", config.channelTitle)
+    channelElement.elements.push(titleElement);
+    //add siteLink
+    let siteLinkElement = textElement("link", config.siteLink)
+    channelElement.elements.push(siteLinkElement);
+    // add description if provided
+    if (config.channelDescription && config.channelDescription.length !== 0){
+        let descriptionElement = textElement("description", config.channelDescription);
+        channelElement.elements.push(descriptionElement);
+    }
+    // add language if provided
+    if (config.channelLanguage && config.channelLanguage.length === 2) {
+        let languageElement = textElement("language", config.channelLanguage);
+        channelElement.elements.push(languageElement);
+    }
+    // add items
+    const generateItem = (feedItem) => {
+        let item = {
+            type: "element",
+            name: "item",
+            elements: []
+        }
+        //add title
+        let titleElement = textElement("title", feedItem.title);
+        item.elements.push(titleElement);
+        //add link
+        let linkElement = textElement("link", feedItem.link);
+        item.elements.push(linkElement);
+        return item;
+    }
+    for (item of items){
+        let itemElement = generateItem(item);
+        channelElement.elements.push(itemElement);
+    }
+    feed.elements[0].elements.push(channelElement);
+    //console.debug(JSON.stringify(feed, null, 4));
+    return feed;
 }
 
 function filterContent(content){
---
+++ test/feed_generation.test.js
@@ -0,0 +1,116 @@
+const zenFeed = require('../index');
+
+const sampleItems = [
+    {
+        title: "Test publication",
+        link: "http://testsite.site/test",
+        pdalink: "http://m.testsite.site/test",
+        amplink: "http://testsite.site/amp/test",
+        media_rating: "nonadult",
+        pubDate: (new Date("July 20, 69 00:20:18 GMT+00:00")).getTime(),
+        author: "Test author",
+        category: [
+            "Технологии",
+            "Наука"
+        ],
+        enclosure: [
+            "http://testsite.site/imgs/test.jpg"
+        ],
+        desription: "Test description",
+        content_encoded: `<![CDATA[
+            Test publication text
+            ]]>`
+    }
+];
+
+const config = {
+    rssVersion: "2.0",
+    channelTitle: "Test channel title",
+    siteLink: "http://testsite.test",
+    channelDescription: "Test channel description",
+    channelLanguage: "ru",
+    getFeedContent: async () => {
+        return sampleItems;
+    },
+    useParser: false
+}
+
+describe('Generating feed object', () => {
+    let rssObject;
+    let channelObject;
+    beforeAll(() => {
+        rssObject = zenFeed.generateFeed(config, sampleItems);
+        channelObject = rssObject.elements[0].elements[0];
+    })
+    describe('channel info', () => {
+        test('rss', () => {
+            expect(rssObject['elements'][0]['name']).toBe('rss');
+        });
+        test('rss version', () => {
+            expect(rssObject['elements'][0]['attributes']['version']).toBe(config.rssVersion);
+        });
+        describe('channel element', () => {
+            test('channel element present', () => {
+                expect(rssObject.elements[0].elements.filter(item => item.name === "channel").length).toBe(1);
+            });
+            test('title', () => {
+                expect(channelObject.elements.find(item => item.name === "title").elements[0].text).toBe(config.channelTitle);
+            });
+            test('link', () => {
+                expect(channelObject.elements.find(item => item.name === "link").elements[0].text).toBe(config.siteLink);
+            });
+            test('description (if present)', () => {
+                if (config.channelDescription) {
+                    expect(channelObject.elements.find(item => item.name === "description").elements[0].text).toBe(config.channelDescription);
+                };
+            });
+            test('no description element if description not provided', () => {
+                let newConfig = Object.assign({}, config);
+                delete(newConfig.channelDescription);
+                let newFeedObject = zenFeed.generateFeed(newConfig, sampleItems);
+                let newChannelObject = newFeedObject.elements[0].elements[0];
+                expect(newChannelObject.elements.filter(item => item.name === "description").length).toBe(0);
+            });
+            test('language (if present)', () => {                
+                if (config.channelLanguage) {
+                    expect(channelObject.elements.find(item => item.name === "language").elements[0].text).toBe(config.channelLanguage);
+                }
+            });
+            test('no language element if language not provided', () => {
+                let newConfig = Object.assign({}, config);
+                delete(newConfig.channelLanguage);
+                let newFeedObject = zenFeed.generateFeed(newConfig, sampleItems);
+                let newChannelObject = newFeedObject.elements[0].elements[0];
+                expect(newChannelObject.elements.filter(item => item.name === "language").length).toBe(0);
+            });
+            test('item - should be at least one', () => {
+                expect(channelObject.elements.filter(item => item.name === "item").length).toBeGreaterThanOrEqual(1);
+            });
+        });
+        describe('items', () => {
+            // let items;
+            // beforeAll(() => {
+            //     items = channelObject.elements.filter(item => item.name === "item");
+            // })
+            // const checkForSingleElement = (item, name) => {
+            //     if (item.elements.filter(el => el.name === name).length === 1) return true;
+            //         else return false;
+            // }
+            // test('title', () => {
+            //     expect(items.every(item => checkForSingleElement(item, "title"))).toBeTruthy();
+            // });
+            // test('link', () => {
+            //     expect(items.every(item => checkForSingleElement(item, "link"))).toBeTruthy();
+            // });
+            // test.todo('pdalink (if present)');
+            // test.todo('amplink (if present)');
+            // test.todo('media:rating (if present; should be ONLY "adult" | "nonadult")');
+            // test.todo('pubDate');
+            // test.todo('author');
+            // test.todo('category (if present; should contain only items from categoryList)');
+            // test.todo('enclosure (should contain at least one element)');
+            // test.todo('description (if present)');
+            // test.todo('content:encoded');
+        })
+    })
+});
